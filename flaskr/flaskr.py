e99030683b267b54cad8470c5732bb628ee80680
ltr0121 <ttrrr121@gmail.com>
2019-03-18 19:23:48 +0900

    flask.py 데이터베이스 연결하는 함수

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -4,6 +4,26 @@ from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+        current application context.
+        """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the databhase again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
 app = Flask(__name__) # create the application instance :)
 app.config.from_object(__name__) # load config from this file , flaskr.py
 
@@ -15,3 +35,4 @@ app.config.update(dict(
     PASSWORD='default'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
